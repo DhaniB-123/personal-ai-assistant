f7e76e6d95f55606b7df4ee7b24f7bf646f458bd
Dhani_Baksh <generativeaiandchatbot@gmail.com>
2026-09-12 16:32:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 from routers import chat
 import models
 from database import base,engine
+from fastapi.middleware.cors import CORSMiddleware
 
 base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(chat.router)
 
